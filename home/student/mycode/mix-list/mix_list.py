4bb811f9f7194815d9087fa6359f3b4d9b684946
git2027 <csande2027@gmail.com>
2020-11-09 20:19:30 +0000

    working with data structures

---
+++ home/student/mycode/mix-list/mix_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+my_list = [ "192.168.0.5", 5060, "UP" ]
+print("the first item in my list (IP):"+my_list[0])
+print("the second item in my list (IP):"+str(my_list[1]))
+print("the last item in my list (IP):"+my_list[2])
+new_list = [5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh"]
+print("the IP addresses are:"+new_list[3] + str(" and ") + new_list[4])
+
